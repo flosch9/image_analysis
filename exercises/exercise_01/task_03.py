b2f7ab1b93546600eb026945bc58d89db051bdd3
flo <fschieren5@gmail.com>
2024-01-25 11:07:20 +0100

    task3 finished

--- exercises/exercise_01/task_03.py
+++ exercises/exercise_01/task_03.py
@@ -49,8 +49,44 @@ print("\n Task 3b")
 shape_of_array = np.shape(picture_array)
 print("Shape of array (equals Image seize):", shape_of_array)
 
+rows, columns = shape_of_array
+
 # Task 3c
 print("\n Task 3c")
+
+
+grey_scale = np.unique(picture_array)
+
+colored_picture_array = [[0]*columns]*rows
+
+band_one = ImageColor.getrgb("green")
+band_two = ImageColor.getrgb("red")
+band_three = ImageColor.getrgb("orange")
+band_four = ImageColor.getrgb("yellow")
+band_five = ImageColor.getrgb("pink")
+band_six = ImageColor.getrgb("black")
+
+# np.array cant be used jet bcs np.aary deosnt take tupel as input
+for row in range(rows):
+    for column in range(columns):
+        if picture_array[row][column] == grey_scale[0]:
+            colored_picture_array[row][column] = band_one
+        elif picture_array[row][column] == grey_scale[1]:
+            colored_picture_array[row][column] = band_two
+        elif picture_array[row][column] == grey_scale[2]:
+            colored_picture_array[row][column] = band_three
+        elif picture_array[row][column] == grey_scale[3]:
+            colored_picture_array[row][column] = band_four
+        elif picture_array[row][column] == grey_scale[4]:
+            colored_picture_array[row][column] = band_five
+        elif picture_array[row][column] == grey_scale[5]:
+            colored_picture_array[row][column] = band_six
+
+#colored_picture_array = np.array(colored_picture_array)
+
+plt.imshow(colored_picture_array)
+plt.savefig("exercises\exercise_01\colormap.png")
+plt.show()
 
 
 """
@@ -70,14 +106,8 @@ number_of_rows = shape_of_array[0]
 number_of_columns = shape_of_array[1]
 # should have done this earlier 
 
-grey_scale = np.unique(picture_array)
-
-
-
 colormap = cm.get_cmap(picture_array)
 print(colormap)
-
-
 
 def change_color(pixel):
     switch_case = {
