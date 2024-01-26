23441fef6dbe630bfae73209479c3cb8722d3fb2
flo <fschieren5@gmail.com>
2024-01-26 22:38:34 +0100

    task 3

--- exercises/exercise_02/task_03.py
+++ exercises/exercise_02/task_03.py
@@ -16,15 +16,15 @@ def gamma_transformation(array_of_gamma, plot_gammatransformation = True, adabt_
     for gamma in array_of_gamma:
         s = c*r**gamma
         
+        print("Gammavalue: {}".format(gamma))
         print("Range for r: ({}, {})".format(min(r), max(r)))
-        print("Range for r: ({}, {})".format(min(s), max(s)))
+        print("Range for s: ({}, {})".format(min(s), max(s)))
 
-        s_plot = 256/max(s) * r**gamma
-        if adabt_range == True:
-            s_plot = 256/max(s) * r**gamma
-            plt.plot(r, s_plot)
+        if adabt_range == True: 
+            s_adapted = 256/max(s) * r**gamma
+            plt.plot(r, s_adapted)
         else:
-            plt.plot(r, s_plot)
+            plt.plot(r, s)
 
 
     if plot_gammatransformation == True:        
@@ -40,4 +40,4 @@ gamma_transformation([0.6,0.4,0.3, 1, 1.5, 0.05], plot_gammatransformation = Tru
 # Task 3a
 print("\n Task 3a")
 
-gamma_transformation([0.6,0.4,0.3])
+gamma_transformation([0.6,0.4,0.3, 1, 1.5, 0.05], plot_gammatransformation = True, adabt_range = True)
