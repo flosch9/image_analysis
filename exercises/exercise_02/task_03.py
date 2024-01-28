a9c95665f44915bf03b6d38a07e73d313da12a32
flo <fschieren5@gmail.com>
2024-01-28 10:34:53 +0100

    task 3 and task 4

--- exercises/exercise_02/task_03.py
+++ exercises/exercise_02/task_03.py
@@ -2,32 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 def gamma_transformation(array_of_gamma, plot_gammatransformation = True, adabt_range = False, image = None):
-    r = np.arange(0, 256, step = 1)
-    s = np.arange(0, 256, step = 1)
 
-    if adabt_range == True:
-        c = 1
-        print("Range with 'correction' of range")
-    elif adabt_range == False:
-        c = 1
-        print("Range without 'correction' of range")
+    if image == None:
+        r = np.arange(0, 256, step = 1)
 
-    
     for gamma in array_of_gamma:
+
+        if adabt_range == True:
+            c = 255/max(r**gamma)
+        elif adabt_range == False:
+            c = 1
+
         s = c*r**gamma
         
         print("Gammavalue: {}".format(gamma))
         print("Range for r: ({}, {})".format(min(r), max(r)))
         print("Range for s: ({}, {})".format(min(s), max(s)))
 
-        if adabt_range == True: 
-            s_adapted = 256/max(s) * r**gamma
-            plt.plot(r, s_adapted)
-        else:
-            plt.plot(r, s)
+        plt.plot(r, s, label = "$\gamma$ = {}".format(gamma))
 
-
-    if plot_gammatransformation == True:        
+    if plot_gammatransformation == True: 
+        plt.legend(loc = "best")       
         plt.show()
 
     pass
--- exercises/exercise_02/task_04.py
+++ exercises/exercise_02/task_04.py
@@ -1,21 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-def s_curve_transformation(r, r_zero, E):
-    return((r/r_zero)**E/(1+(r/r_zero**E)))
 
 # Task 4a
 print("\n Task 4a")
+
+def s_curve_transformation(r, r_zero, E):
+    return((r/r_zero)**E/(1+(r/r_zero**E)))
 
 values_of_E = [2,4,6,8]
 r_zero = 0.5
 r = np.linspace(0,1,100)
 
 for E_param in values_of_E:
-    plt.plot(r, s_curve_transformation(r, r_zero, E_param))
-
+    
+    plt.plot(r, s_curve_transformation(r, r_zero, E_param), label = "E = {}, r$_0$ = {}".format(E_param, r_zero) )
+    
+plt.legend(loc = "best")
 plt.show()
 
 
 # Task 4b
 print("\n Task 4b")
+
+def s_curve_transformation_rgbrange(r, r_zero, E):
+    alpha = 255/max((r/r_zero)**E/(1+(r/r_zero**E)))
+    return(alpha*(r/r_zero)**E/(1+(r/r_zero**E)))
+
+r = np.linspace(0,255,255)
+r_zero_values = [0.2,0.7,1.6,5]
+
+
+for E_param in values_of_E:
+    for r_zero in r_zero_values:
+        plt.plot(r, s_curve_transformation_rgbrange(r, r_zero, E_param), label = "E = {}, r$_0$ = {}".format(E_param, r_zero) )
+    
+plt.legend(loc = "best", bbox_to_anchor=(1, 1))
+plt.subplots_adjust(right=0.7)
+plt.show()
+
