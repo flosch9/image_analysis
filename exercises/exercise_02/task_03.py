ce48139bfb82c7e80f1e3b8f0d1b8245f3c8d9f0
flo <fschieren5@gmail.com>
2024-01-30 15:01:39 +0100

    task 03 finisehd 100%

--- exercises/exercise_02/task_03.py
+++ exercises/exercise_02/task_03.py
@@ -1,38 +1,79 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+from PIL import Image
 
 def gamma_transformation(array_of_gamma, plot_gammatransformation = True, adabt_range = False, image = None):
 
     if image == None:
         r = np.arange(0, 256, step = 1)
+    else:
+        r = np.array(image)
+        
+    return_image = []
 
     for gamma in array_of_gamma:
 
         if adabt_range == True:
-            c = 255/max(r**gamma)
+            c = 255/np.amax(r**gamma)
         elif adabt_range == False:
             c = 1
 
         s = c*r**gamma
+        return_image.append(s)
         
         print("\nGammavalue: {}".format(gamma))
-        print("Range for r: ({}, {})".format(min(r), max(r)))
-        print("Range for s: ({}, {})".format(min(s), max(s)))
+        print("Range for r: ({}, {})".format(np.amin(r), np.amax(r)))
+        print("Range for s: ({}, {})".format(np.amin(s), np.amax(s)))
 
         plt.plot(r, s, label = "$\gamma$ = {}".format(gamma))
 
     if plot_gammatransformation == True: 
         plt.legend(loc = "best")       
         plt.show()
-
+    else:
+        plt.close()
+        
+    if image != None:
+        return(return_image)
+    
     pass
 
 # Task 3a
 print("\n Task 3a")
 
-gamma_transformation([0.6,0.4,0.3, 1, 1.5, 0.05], plot_gammatransformation = True, adabt_range = False)
+gamma = [0.6,0.4,0.3,0.1]
 
-# Task 3a
-print("\n Task 3a")
+gamma_transformation(gamma, plot_gammatransformation = True, adabt_range = False)
 
-gamma_transformation([0.6,0.4,0.3, 1, 1.5, 0.05], plot_gammatransformation = True, adabt_range = True)
+image = Image.open("data\DIP3E_Original_Images_CH03\Fig0308(a)(fractured_spine).tif")
+
+transformed_image = gamma_transformation(gamma, plot_gammatransformation = False, adabt_range = False, image = image)
+
+fig, axes = plt.subplots(nrows=1, ncols=len(gamma)+1, figsize=(8, 5))
+axes[0].imshow(image, cmap = "gray")
+axes[0].set_title("Original")
+for i, gamma in enumerate(gamma):
+    axes[i+1].imshow(transformed_image[i], cmap = "gray")
+    axes[i+1].set_title("$\gamma$ = {}".format(gamma))
+fig.tight_layout()
+plt.show()
+
+
+# Task 3b
+print("\n Task 3b")
+
+gamma = [0.6,0.4,0.3]
+
+gamma_transformation(gamma, plot_gammatransformation = True, adabt_range = True)
+
+transformed_image = gamma_transformation(gamma, plot_gammatransformation = False, adabt_range = True, image = image)
+
+fig, axes = plt.subplots(nrows=1, ncols=len(gamma)+1, figsize=(8, 5))
+axes[0].imshow(image, cmap = "gray")
+axes[0].set_title("Original")
+for i, gamma in enumerate(gamma):
+    axes[i+1].imshow(transformed_image[i], cmap = "gray")
+    axes[i+1].set_title("$\gamma$ = {}".format(gamma))
+
+fig.tight_layout()
+plt.show()
